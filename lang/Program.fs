ec04d9dc1e6e4fd820872cb05a34109dc685a409
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2020-05-19 02:10:24 -0400

    Fix error handling of program

--- lang/Program.fs
+++ lang/Program.fs
@@ -6,13 +6,12 @@ open ProjectInterpreter
 
 [<EntryPoint>]
 let main argv =
-    //If there are going to be newline characters in the input, it cannot be run via dotnet run, because they are not read in properly
-    //Rather, the program has to be generated using the interface
     try
        let input = formatInputString argv.[0]
        match parse input with
        |Some r -> eval r |> eprettyprint |> printfn "%s"
        |None -> failwith "Invalid Expression"
     with
-    |_ -> printf "Usage: dotnet run <program>\n<program> is a string representation of the program to be run\n"
+       | :? IndexOutOfRangeException -> printf "Usage: dotnet run <program>\n<program> is a string representation of the program to be run\n"
+       | e -> printfn "%s" e.Message
     0 // return an integer exit code
